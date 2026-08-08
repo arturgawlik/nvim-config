4967b29670f9260418adde4d0aa66e2411e7fb11
Artur Gawlik <artur.gawlik@fadv.com>
2026-08-08 15:12:19 +0200

    restore sessions for given cwd

---
+++ lua/plugins/auto_restore_sessions.lua
@@ -0,0 +1,27 @@
+-- configured by me based on
+-- https://docs.astronvim.com/recipes/sessions/#automatically-restore-previous-session
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    autocmds = {
+      restore_session_on_open = {
+        {
+          event = "VimEnter",
+          desc = "Restore previous directory session if neovim opened with no arguments",
+          nested = true, -- trigger other autocommands as buffers open
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+              -- try to load a directory session using the current working directory
+              require("resession").load(
+                vim.fn.getcwd(),
+                { dir = "dirsession", silence_errors = true }
+              )
+            end
+          end,
+        },
+      },
+    },
+  },
+}
